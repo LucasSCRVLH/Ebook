750af9208f4080d15d97af15e95254a5f251a3ae
Lucas Carvalho <blujack11@gmail.com>
2024-09-16 21:55:40 -0300

    Envio da criação das pastas do projeto

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,18 @@
+
+  export function Home(){
+
+        return ( 
+        <header> 
+            
+            <ul className="aside"> 
+                <li>home</li>
+                <li>fav</li>
+                <li>pedidos</li>
+            </ul>
+
+        </header> )
+
+
+
+  }
+
